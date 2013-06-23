12c0e41697ae04b69435315f27f5eab7952522bf
Michel Weimerskirch <michel@weimerskirch.net>
2013-06-23 13:31:28 +0300

    Removed broken Content-Encoding header
    Updated version of PR #7 for version 4.0.

--- includes/SpellChecker.php
+++ includes/SpellChecker.php
@@ -65,8 +65,7 @@ class TinyMCE_SpellChecker {
 		$engine = new $engine();
 		$engine->setConfig($tinymceSpellcheckerConfig);
 
-		header('Content-Type: application/json');
-		header('Content-Encoding: UTF-8');
+		header("Content-Type: application/json; charset=utf-8");
 		header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 		header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 		header("Cache-Control: no-store, no-cache, must-revalidate");
