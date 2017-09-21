53bb265c9537fdbd2203e5fa6930c3d6249962a6
Nicolas <NVasse@users.noreply.github.com>
2017-09-21 12:54:27 +0200

    Added fr
    Added fr to normalized lang codes
    Update PHPDoc

--- includes/EnchantEngine.php
+++ includes/EnchantEngine.php
@@ -9,82 +9,99 @@
  * Contributing: http://www.tinymce.com/contributing
  */
 
-class TinyMCE_SpellChecker_EnchantEngine extends TinyMCE_SpellChecker_Engine {
-	/**
-	 * Spellchecks an array of words.
-	 *
-	 * @param String $lang Selected language code (like en_US or de_DE). Shortcodes like "en" and "de" work with enchant >= 1.4.1
-	 * @param Array $words Array of words to check.
-	 * @return Name/value object with arrays of suggestions.
-	 */
-	public function getSuggestions($lang, $words) {
-		$suggestions = array();
-		$enchant = enchant_broker_init();
-		$config = $this->getConfig();
+/**
+ * Class TinyMCE_SpellChecker_EnchantEngine
+ */
+class TinyMCE_SpellChecker_EnchantEngine extends TinyMCE_SpellChecker_Engine
+{
+    /**
+     * Spellchecks an array of words.
+     *
+     * @param String $lang  Selected language code (like en_US or de_DE). Shortcodes like "en" and "de" work with
+     *                      enchant >= 1.4.1
+     * @param array  $words Array of words to check.
+     *
+     * @return array arrays of suggestions.
+     * @throws Exception
+     */
+    public function getSuggestions($lang, $words)
+    {
+        $suggestions = [];
+        $enchant = enchant_broker_init();
+        $config = $this->getConfig();
 
-		if (isset($config["enchant_dicts_path"])) {
-			enchant_broker_set_dict_path($enchant, ENCHANT_MYSPELL, $config["enchant_dicts_path"]);
-			enchant_broker_set_dict_path($enchant, ENCHANT_ISPELL, $config["enchant_dicts_path"]);
-		}
+        if (isset($config["enchant_dicts_path"])) {
+            enchant_broker_set_dict_path($enchant, ENCHANT_MYSPELL, $config["enchant_dicts_path"]);
+            enchant_broker_set_dict_path($enchant, ENCHANT_ISPELL, $config["enchant_dicts_path"]);
+        }
 
-		if (!enchant_broker_describe($enchant)) {
-			throw new Exception("Enchant spellchecker not find any backends.");
-		}
+        if (!enchant_broker_describe($enchant)) {
+            throw new Exception("Enchant spellchecker not find any backends.");
+        }
 
-		$lang = $this->normalizeLangCode($enchant, $lang);
+        $lang = $this->normalizeLangCode($enchant, $lang);
 
-		if (enchant_broker_dict_exists($enchant, $lang)) {
-			$dict = enchant_broker_request_dict($enchant, $lang);
+        if (enchant_broker_dict_exists($enchant, $lang)) {
+            $dict = enchant_broker_request_dict($enchant, $lang);
 
-			foreach ($words as $word) {
-				if (!enchant_dict_check($dict, $word)) {
-					$suggs = enchant_dict_suggest($dict, $word);
+            foreach ($words as $word) {
+                if (!enchant_dict_check($dict, $word)) {
+                    $suggs = enchant_dict_suggest($dict, $word);
 
-					if (!is_array($suggs)) {
-						$suggs = array();
-					}
+                    if (!is_array($suggs)) {
+                        $suggs = [];
+                    }
 
-					$suggestions[$word] = $suggs;
-				}
-			}
+                    $suggestions[$word] = $suggs;
+                }
+            }
 
-			enchant_broker_free_dict($dict);
-			enchant_broker_free($enchant);
-		} else {
-			enchant_broker_free($enchant);
-			throw new Exception("Enchant spellchecker could not find dictionary for language: " . $lang);
-		}
+            enchant_broker_free_dict($dict);
+            enchant_broker_free($enchant);
+        } else {
+            enchant_broker_free($enchant);
+            throw new Exception("Enchant spellchecker could not find dictionary for language: " . $lang);
+        }
 
-		return $suggestions;
-	}
+        return $suggestions;
+    }
 
-	/**
-	 * Return true/false if the engine is supported by the server.
-	 *
-	 * @return boolean True/false if the engine is supported.
-	 */
-	public function isSupported() {
-		return function_exists("enchant_broker_init");
-	}
+    /**
+     * Return true/false if the engine is supported by the server.
+     *
+     * @return boolean True/false if the engine is supported.
+     */
+    public function isSupported()
+    {
+        return function_exists("enchant_broker_init");
+    }
 
-	private function normalizeLangCode($enchant, $lang) {
-		$variants = array(
-			"en" => array("en_US", "en_GB")
-		);
+    /**
+     * @param $enchant
+     * @param $lang
+     *
+     * @return mixed
+     */
+    private function normalizeLangCode($enchant, $lang)
+    {
+        $variants = [
+            "en" => ["en_US", "en_GB"],
+            "fr" => ["fr_FR"],
+        ];
 
-		if (isset($variants[$lang])) {
-			array_unshift($variants, $lang);
+        if (isset($variants[$lang])) {
+            array_unshift($variants, $lang);
 
-			foreach ($variants[$lang] as $variant) {
-				if (enchant_broker_dict_exists($enchant, $variant)) {
-					return $variant;
-				}
-			}
-		}
+            foreach ($variants[$lang] as $variant) {
+                if (enchant_broker_dict_exists($enchant, $variant)) {
+                    return $variant;
+                }
+            }
+        }
 
-		return $lang;
-	}
+        return $lang;
+    }
 }
 
-TinyMCE_Spellchecker_Engine::add("enchant", "TinyMCE_SpellChecker_EnchantEngine");
+TinyMCE_SpellChecker_Engine::add("enchant", "TinyMCE_SpellChecker_EnchantEngine");
 ?>
