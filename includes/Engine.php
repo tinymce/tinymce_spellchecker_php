d1d5635a11b09f147cfa3aae23144d8dce3a3e02
Nicolas <NVasse@users.noreply.github.com>
2017-09-21 12:53:23 +0200

    Added get params from request payload
    Update PHPDoc
    Update getParam method to retrieve from request payload

--- includes/Engine.php
+++ includes/Engine.php
@@ -12,161 +12,200 @@
  * spelling on and returns the suggestions.
  */
 
-class TinyMCE_SpellChecker_Engine {
-	private static $engines = array();
-	private $config;
+/**
+ * Class TinyMCE_SpellChecker_Engine
+ */
+class TinyMCE_SpellChecker_Engine
+{
+    private static $engines = [];
+    private $config;
 
-	public function __constructor($config) {
-		$this->config = $config;
-	}
+    public function __constructor($config)
+    {
+        $this->config = $config;
+    }
 
-	/**
-	 * Spellchecks an array of words.
-	 *
-	 * @param String $lang Selected language code (like en_US or de_DE). Shortcodes like "en" and "de" work with enchant >= 1.4.1
-	 * @param Array $words Array of words to check.
-	 * @return Name/value object with arrays of suggestions.
-	 */
-	public function getSuggestions($lang, $words) {
-		return array();
-	}
+    /**
+     * Spellchecks an array of words.
+     *
+     * @param String $lang  Selected language code (like en_US or de_DE). Shortcodes like "en" and "de" work with
+     *                      enchant >= 1.4.1
+     * @param array  $words Array of words to check.
+     *
+     * @return array array of suggestions.
+     */
+    public function getSuggestions($lang, $words)
+    {
+        return [];
+    }
 
-	/**
-	 * Return true/false if the engine is supported by the server.
-	 *
-	 * @return boolean True/false if the engine is supported.
-	 */
-	public function isSupported() {
-		return true;
-	}
+    /**
+     * Return true/false if the engine is supported by the server.
+     *
+     * @return boolean True/false if the engine is supported.
+     */
+    public function isSupported()
+    {
+        return true;
+    }
 
-	/**
-	 * Sets the config array used to create the instance.
-	 *
-	 * @param Array $config Name/value array with config options.
-	 */
-	public function setConfig($config) {
-		$this->config = $config;
-	}
+    /**
+     * Sets the config array used to create the instance.
+     *
+     * @param array $config Name/value array with config options.
+     */
+    public function setConfig($config)
+    {
+        $this->config = $config;
+    }
 
-	/**
-	 * Returns the config array used to create the instance.
-	 *
-	 * @return Array Name/value array with config options.
-	 */
-	public function getConfig() {
-		return $this->config;
-	}
+    /**
+     * Returns the config array used to create the instance.
+     *
+     * @return array Name/value array with config options.
+     */
+    public function getConfig()
+    {
+        return $this->config;
+    }
 
-	// Static methods
+    // Static methods
 
-	public static function processRequest($tinymceSpellcheckerConfig) {
-		$engine = self::get($tinymceSpellcheckerConfig["engine"]);
-		$engine = new $engine();
-		$engine->setConfig($tinymceSpellcheckerConfig);
+    /**
+     * @param $tinymceSpellcheckerConfig
+     */
+    public static function processRequest($tinymceSpellcheckerConfig)
+    {
+        $engine = self::get($tinymceSpellcheckerConfig["engine"]);
+        $engine = new $engine();
+        $engine->setConfig($tinymceSpellcheckerConfig);
 
-		header('Content-Type: application/json');
-		header('Content-Encoding: UTF-8');
-		header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-		header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
-		header("Cache-Control: no-store, no-cache, must-revalidate");
-		header("Cache-Control: post-check=0, pre-check=0", false);
-		header("Pragma: no-cache");
+        header('Content-Type: application/json');
+        header('Content-Encoding: UTF-8');
+        header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+        header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+        header("Cache-Control: no-store, no-cache, must-revalidate");
+        header("Cache-Control: post-check=0, pre-check=0", false);
+        header("Pragma: no-cache");
 
-		$method = self::getParam("method", "spellcheck");
-		$lang = self::getParam("lang", "en_US");
-		$text = self::getParam("text");
+        $method = self::getParam("method", "spellcheck");
+        $lang = self::getParam("lang", "en_US");
+        $text = self::getParam("text");
 
-		if ($method == "spellcheck") {
-			try {
-				if (!$text) {
-					throw new Exception("Missing input parameter 'text'.");
-				}
+        if ($method == "spellcheck") {
+            try {
+                if (!$text) {
+                    throw new Exception("Missing input parameter 'text'.");
+                }
 
-				if (!$engine->isSupported()) {
-					throw new Exception("Current spellchecker isn't supported.");
-				}
+                if (!$engine->isSupported()) {
+                    throw new Exception("Current spellchecker isn't supported.");
+                }
 
-				$words = self::getWords($text);
+                $words = self::getWords($text);
 
-				echo json_encode((object) array(
-					"words" => (object) $engine->getSuggestions($lang, $words)
-				));
-			} catch (Exception $e) {
-				echo json_encode((object) array(
-					"error" => $e->getMessage()
-				));
-			}
-		} else if ($method) {
-			echo json_encode((object) array(
-				"error" => "Unsupported method: " . $method
-			));
-		} else {
-			echo json_encode((object) array(
-				"error" => "Invalid JSON input"
-			));
-		}
-	}
+                echo json_encode(
+                    (object)[
+                        "words" => (object)$engine->getSuggestions($lang, $words),
+                    ]
+                );
+            } catch (Exception $e) {
+                echo json_encode(
+                    (object)[
+                        "error" => $e->getMessage(),
+                    ]
+                );
+            }
+        } else {
+            echo json_encode(
+                (object)[
+                    "error" => "Invalid JSON input",
+                ]
+            );
+        }
+    }
 
-	/**
-	 * Returns an request value by name without magic quoting.
-	 *
-	 * @param String $name Name of parameter to get.
-	 * @param String $default_value Default value to return if value not found.
-	 * @return String request value by name without magic quoting or default value.
-	 */
-	public static function getParam($name, $default_value = false) {
-		if (isset($_POST[$name])) {
-			$req = $_POST;
-		} else if (isset($_GET[$name])) {
-			$req = $_GET;
-		} else {
-			return $default_value;
-		}
+    /**
+     * Returns an request value by name without magic quoting.
+     *
+     * @param String      $name          Name of parameter to get.
+     * @param bool|String $default_value Default value to return if value not found.
+     *
+     * @return String request value by name without magic quoting or default value.
+     */
+    public static function getParam($name, $default_value = false)
+    {
+        $requestPayload = json_decode(file_get_contents('php://input'), true);
+        if (!empty($_POST[$name])) {
+            $req = $_POST;
+        } elseif (!empty($_GET[$name])) {
+            $req = $_GET;
+        } elseif (!empty($requestPayload['params']) && !empty($requestPayload['params'][$name])) {
+            $req = $requestPayload['params'];
+        } else {
+            return $default_value;
+        }
 
-		// Handle magic quotes
-		if (ini_get("magic_quotes_gpc")) {
-			if (is_array($req[$name])) {
-				$out = array();
+        // Handle magic quotes
+        if (ini_get("magic_quotes_gpc")) {
+            if (is_array($req[$name])) {
+                $out = [];
 
-				foreach ($req[$name] as $name => $value) {
-					$out[stripslashes($name)] = stripslashes($value);
-				}
+                foreach ($req[$name] as $name => $value) {
+                    $out[stripslashes($name)] = stripslashes($value);
+                }
 
-				return $out;
-			}
+                return $out;
+            }
 
-			return stripslashes($req[$name]);
-		}
+            return stripslashes($req[$name]);
+        }
 
-		return $req[$name];
-	}
+        return $req[$name];
+    }
 
-	public static function add($name, $className) {
-		self::$engines[$name] = $className;
-	}
+    /**
+     * @param $name
+     * @param $className
+     */
+    public static function add($name, $className)
+    {
+        self::$engines[$name] = $className;
+    }
 
-	public static function get($name) {
-		if (!isset(self::$engines[$name])) {
-			return null;
-		}
+    /**
+     * @param $name
+     *
+     * @return mixed|null
+     */
+    public static function get($name)
+    {
+        if (!isset(self::$engines[$name])) {
+            return null;
+        }
 
-		return self::$engines[$name];
-	}
+        return self::$engines[$name];
+    }
 
-	public static function getWords($text) {
-		preg_match_all('(\w{3,})u', $text, $matches);
-		$words = $matches[0];
+    /**
+     * @param $text
+     *
+     * @return mixed
+     */
+    public static function getWords($text)
+    {
+        preg_match_all('(\w{3,})u', $text, $matches);
+        $words = $matches[0];
 
-		for ($i = count($words) - 1;  $i >= 0; $i--) {
-			// Exclude words with numbers in them
-			if (preg_match('/[0-9]+/', $words[$i])) {
-				array_splice($words, $i, 1);
-			}
-		}
+        for ($i = count($words) - 1; $i >= 0; $i--) {
+            // Exclude words with numbers in them
+            if (preg_match('/[0-9]+/', $words[$i])) {
+                array_splice($words, $i, 1);
+            }
+        }
 
-		return $words;
-	}
+        return $words;
+    }
 }
 
 ?>
